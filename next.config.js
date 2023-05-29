f41db7bbd123d97fa30d3162df973e0dc572b833
Abdul Ibrahim <theoneadisa@gmail.com>
2023-05-29 13:58:19 +0100

    env

--- next.config.js
+++ next.config.js
@@ -17,15 +17,15 @@ module.exports = withTM({
   swcMinify: false,
   trailingSlash: true,
   env: {
-    HOST_API_KEY: 'https://minimal-assets-api.vercel.app',
+    HOST_API_KEY: '',
     // FIREBASE AUTH
-    FIREBASE_API_KEY: 'AIzaSyDEYrbJhYgiFju3p8vWhpLed72T727XPvY',
-    FIREBASE_AUTH_DOMAIN: 'powerstack-47e85.firebaseapp.com',
-    FIREBASE_PROJECT_ID: 'powerstack-47e85',
-    FIREBASE_STORAGE_BUCKET: 'powerstack-47e85.appspot.com',
-    FIREBASE_MESSAGING_SENDER_ID: '118004188258',
-    FIREBASE_APPID: '1:118004188258:web:7b76c4817a009d645abd3b',
-    FIREBASE_MEASUREMENT_ID: 'G-FKZNED6QGP',
+    FIREBASE_API_KEY: '',
+    FIREBASE_AUTH_DOMAIN: '',
+    FIREBASE_PROJECT_ID: '',
+    FIREBASE_STORAGE_BUCKET: '',
+    FIREBASE_MESSAGING_SENDER_ID: '',
+    FIREBASE_APPID: '',
+    FIREBASE_MEASUREMENT_ID: '',
     // AWS COGNITO AUTH
     AWS_COGNITO_USER_POOL_ID: '',
     AWS_COGNITO_CLIENT_ID: '',
